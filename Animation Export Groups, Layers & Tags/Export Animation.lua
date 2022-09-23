b850e881fb4fe8497810af3bc6dcc4cf4b823ec9
Alexander Harmon <wolfaceking@gmail.com>
2022-09-23 14:10:27 -0700

    Fixed issue with duplicate exports
    Fixed issue with only the top most layer being exported in an export.

--- Animation Export Groups, Layers & Tags/Export Animation.lua
+++ Animation Export Groups, Layers & Tags/Export Animation.lua
@@ -1,8 +1,9 @@
 -- Export each tag into a different sprite sheet
 
 function exportLayerAndTag(tempLayer, tempTag, tempColumnNumbers, tempFileName)
-    app.command.ExportSpriteSheet{ 
-		ui=false,						
+    app.command.ExportSpriteSheet{
+		ui=false,
+		askOverwrite=false,
 		type=SpriteSheetType.Rows,  	-- Change this if you want to change how the exported sprite is displayed.
 		columns=tempColumnNumbers,    	-- Use one of these options(HORIZONTAL, VERTICAL, ROWS, COLUMNS, PACKED)
 		rows=tempColumnNumbers,			-- You can learn more here
@@ -10,7 +11,7 @@ function exportLayerAndTag(tempLayer, tempTag, tempColumnNumbers, tempFileName)
 		textureFilename=tempFileName .. '.png',
 		-- dataFilename=fn .. '.json',
 		dataFormat=SpriteSheetDataFormat.JSON_ARRAY,
-	    borderPadding=0,
+	  borderPadding=0,
 		shapePadding=0,
 		innerPadding=0,
 		trimSprite=false,
@@ -27,6 +28,7 @@ function exportLayerAndTag(tempLayer, tempTag, tempColumnNumbers, tempFileName)
 		listLayers=false,
 		listTags=false,
 		listSlices=false,
+		fromTilesets=false,
 	}
 end
 
@@ -48,14 +50,14 @@ end
 
 function exportSpriteSheets(tempLayers, tempTags, tempPath)
 	local numberOfColumns
-	
+
 	for i,tag in ipairs(tempTags) do
 	  for j, layer in ipairs(tempLayers) do
 		if layer.layers ~= nil then --if layer is a group then loop through all layers in it.
 			for k, groupedLayer in ipairs(layer.layers) do
 				fileName = tempPath .. '/' .. fileNameWithGroups(layer.name, groupedLayer.name, tag.name)
 				numberOfColumns = math.ceil(math.sqrt(tag.frames))--calculates the best way place the sprites in a square
-				exportLayerAndTag(layer, tag, numberOfColumns, fileName)
+				exportLayerAndTag(groupedLayer, tag, numberOfColumns, fileName)
 			end
 		else
 			fileName = tempPath .. '/' .. fileNameWithoutGroups(layer.name, tag.name)
@@ -70,6 +72,7 @@ end
 function fileNameWithGroups(groupName, layerName, tagName)
 return groupName .. typeOfLetterSpacing .. layerName .. typeOfLetterSpacing .. tagName
 end
+
 function fileNameWithoutGroups(layerName, tagName)
 return layerName .. typeOfLetterSpacing .. tagName
 end
@@ -85,10 +88,8 @@ local spriteLayers = spr.layers
 local msg = { "Do you want to export/overwrite the following files?" }
 fileName = nil
 
-
 -- Change this value for what type of spacing you want between group,layer and tag names.
 typeOfLetterSpacing = '_'
-
 
 table.insert(msg, checkIfDuplicate(spriteLayers, spr.tags, path))
 
@@ -98,7 +99,3 @@ if app.alert{ title="Export Sprite Sheets", text=msg,
 end
 
 exportSpriteSheets(spriteLayers, spr.tags, path)
-
-
-
-
